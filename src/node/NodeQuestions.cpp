ea60a72cf0a835eb2bfb67b71ed7ae4862677037
jedsaxon <jedsaxon@gmail.com>
2023-10-02 12:39:28 +1100

    Added even more documentaiton beacuh,aecrhcluhcra,.uhbcrl,.pau

--- src/node/NodeQuestions.cpp
+++ src/node/NodeQuestions.cpp
@@ -1,85 +1,118 @@
-// This C++ header file defines a generic 'NodeList' class, which is used to manage a list of nodes.
-// It includes functions for inserting nodes, accessing the first and last nodes, and getting the size of the list.
+// This C++ code appears to be a console-based program that manages a list of contacts using a custom class 'Node' and 'ContactList'.
+// Users can insert, list, search, and perform other operations on contacts.
 
-#ifndef ADVANCED_PROGRAMMINGS_AT1_NODELIST_H
-#define ADVANCED_PROGRAMMINGS_AT1_NODELIST_H
+#include <iostream>            // Include the input/output stream library.
+#include "NodeQuestions.h"     // Include the header file for NodeQuestions class.
+#include "ContactList.h"       // Include the header file for the ContactList class.
 
-// Include necessary headers.
-#include <iostream>     // Include the input/output stream for debugging.
-#include <memory>       // Include the memory header for smart pointers.
-#include "Node.h"       // Include the Node class definition.
+// Implementation of the Start function in the NodeQuestions class.
+int NodeQuestions::Start() {
+    // Create several 'Node' objects with 'Contact' data and set up a 'ContactList'.
+    Node<Contact>* node4 = new Node<Contact>(Contact("Foo4", "Bar4", 60));
+    Node<Contact>* node2 = new Node<Contact>(Contact("Foo2", "Bar2", 40));
+    Node<Contact>* node5 = new Node<Contact>(Contact("Foo5", "Bar5", 70));
+    Node<Contact>* node1 = new Node<Contact>(Contact("Foo1", "Bar1", 30));
+    Node<Contact>* node3 = new Node<Contact>(Contact("Foo3", "Bar3", 50));
+    Node<Contact>* node6 = new Node<Contact>(Contact("Foo6", "Bar6", 80));
 
-// Define a generic NodeList class using a template.
-template <typename T>
-class NodeList {
-private:
-    Node<T>* first;     // Pointer to the first node in the list.
-    Node<T>* last;      // Pointer to the last node in the list.
-    int size;           // The size of the list (number of nodes).
+    ContactList* list = new ContactList(); // Create a ContactList.
 
-    // Private member function to recursively destroy nodes starting from the given node.
-    void DestroyNodeRecursive(Node<T> *firstNode) {
-        /* If there is a previous node, it will call this same function to delete it. If that node also has a previous
-         * one, it will be deleted too. This means that this function will end up being called for each node in the
-         * list. Once it reaches the end of the list, it will delete the node and then get out of the function, meaning
-         * the node in front of it will be deleted, and it goes on until all nodes have been deleted
-         */
+    // Insert the created nodes into the ContactList.
+    list->Insert(node4);
+    list->Insert(node2);
+    list->Insert(node5);
+    list->Insert(node1);
+    list->Insert(node3);
+    list->Insert(node6);
 
-        if (firstNode->GetPreviousNode()) {
-            DestroyNodeRecursive(firstNode->GetPreviousNode());
+    // Display a menu for user interaction.
+    std::cout << "Select Option" << std::endl
+              << "1: Insert Element" << std::endl
+              << "2: Loop Through Elements" << std::endl
+              << "3: Loop Through Elements in Reverse" << std::endl
+              << "4: Search" << std::endl
+              << "5: [Deprecated] Min Age" << std::endl
+              << "6: [Deprecated] Max Age" << std::endl
+              << "7: Sort By Age" << std::endl
+              << "8: Exit" << std::endl;
+
+    while (true) {
+        std::cout << "\n";
+        // Get user input for selecting an option within a specified range (1-8).
+        int option = GetNumberFromUser("Enter Option: ", "Please select a value between 1 and 8", 1, 8);
+        std::cout << "\n";
+
+        switch (option) {
+            case 1: {
+                // Create a new 'Node' using user input and insert it into the ContactList.
+                Node<Contact>* newNode = CreateNode();
+                list->Insert(newNode);
+                continue;
+            }
+            case 2: {
+                // Loop through and display the elements in the ContactList.
+                list->LoopThroughNodes();
+                continue;
+            }
+            case 3: {
+                // Loop through and display the elements in reverse order.
+                list->LoopThroughNodesReverse();
+                continue;
+            }
+            case 4: {
+                // Search for a contact based on age.
+                list->Search();
+                continue;
+            }
+            case 7: {
+                // Sort the contacts by age.
+                std::cout << "Sorting...\n";
+                list->Sort();
+                continue;
+            }
+            case 8: {
+                // Exit the program, releasing memory used by the ContactList.
+                std::cout << "Exiting" << std::endl;
+                list->~ContactList();
+                return 0;
+            }
+            default: {
+                std::cout << "That is an invalid option" << std::endl;
+            }
+        }
+    }
+}
+
+// Helper function to get a valid number input from the user within a specified range.
+int NodeQuestions::GetNumberFromUser(const std::string &message, const std::string &errorMessage, int min, int max) {
+    while (true) {
+        int option = 0;
+        std::cout << message;
+        std::cin >> option;
+
+        if (option < min || option > max) {
+            std::cout << errorMessage << std::endl;
+            continue;
         }
 
-        // Delete the firstNode after deleting the previous one.
-        std::cout << "Deleting Node";
-        delete firstNode;
+        return option;
     }
+}
 
-    // Private member function to destroy all nodes in the list.
-    void Destroy() {
-        Node<T> *lastNode = GetLast();
-        DestroyNodeRecursive(lastNode);
-    }
+// Helper function to create a new 'Node' with 'Contact' data based on user input.
+Node<Contact>* NodeQuestions::CreateNode() {
+    std::string firstName;
+    std::string lastName;
+    int age;
 
-public:
-    // Destructor that destroys all nodes in the list when the NodeList object is destroyed.
-    ~NodeList() {
-        Destroy();
-    }
+    std::cout << "Inserting New Element\n" << "Enter First Name: ";
+    std::cin >> firstName;
+    std::cout << "Enter Last Name: ";
+    std::cin >> lastName;
 
-    // Function to get a pointer to the first node in the list.
-    Node<T>* GetFirst() { return first; }
+    // Get the age from the user, ensuring it's within a valid range (0-100).
+    age = GetNumberFromUser("Enter Age: ", "Please select a valid age between 0 - 100", 0, 100);
 
-    // Function to get a pointer to the last node in the list.
-    Node<T>* GetLast() { return last; }
-
-    // Function to get the size (number of nodes) in the list.
-    int GetSize() { return size; }
-
-    // Function to insert a new node into the list.
-    void Insert(Node<T>* newNode) {
-        // If nothing is in the list, set the first node (not the last).
-        if (first == nullptr) {
-            first = std::move(newNode);
-            size++;
-            return;
-        }
-
-        // If there's only one element in the list (the first node), set the first node's next node to the new one,
-        // and also set the new one as the last node.
-        if (last == nullptr) {
-            first->SetNextNode(newNode);
-            newNode->SetPreviousNode(first);
-            last = std::move(newNode);
-            size++;
-            return;
-        }
-
-        newNode->SetPreviousNode(last); // Current Last now has one in front of it!
-        last->SetNextNode(newNode);     // Current Last points to the new node.
-        last = newNode;                 // Update the new last node.
-        size++;
-    }
-};
-
-// End of the preprocessor directives.
-#endif //ADVANCED_PROGRAMMINGS_AT1_NODELIST_H
+    // Create a new 'Node' with 'Contact' data and return it.
+    return new Node<Contact>(Contact(firstName, lastName, age));
+}
